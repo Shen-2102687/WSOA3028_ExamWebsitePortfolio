08e85300c485672b1434af7f00869bd5be42a41c
Shen-2102687 <100612118+Shen-2102687@users.noreply.github.com>
2022-07-03 20:54:34 +0200

    updated profile page
    added carousel display, resume, and fixed wireframe text blur

---
+++ Javascript/blogList.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.siteSummary').slick({
+      
+      //accessibility: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      arrows: true,
+      //adaptiveHeight: true,
+    });
+  });
